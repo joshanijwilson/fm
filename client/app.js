cf7dad35145b27102903ed432587c415581f8458
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-05-29 09:05:18 -0700

    Set up routing
    - menu
    - make a reservation
    - existing reservations

--- client/app.js
+++ client/app.js
@@ -1,3 +1,13 @@
-var fm = angular.module('fm', []);
+var fm = angular.module('fm', ['ngRoute']);
 
 fm.controller('App', AppController);
+
+
+// Routing
+fm.config(function($routeProvider) {
+  $routeProvider
+    .when('/',         {templateUrl: './menu.html'})
+    .when('/reserve',  {templateUrl: './reserve.html',  controller: ReserveController})
+    .when('/existing', {templateUrl: './existing.html', controller: ExistingController})
+
+});
