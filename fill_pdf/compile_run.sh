537862b38dad919848de4ee03b58169b2df5e48c
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-06-04 22:40:01 -0700

    fill_pdf: do not run when compile error

--- fill_pdf/compile_run.sh
+++ fill_pdf/compile_run.sh
@@ -2,6 +2,4 @@
 
 DEPS="pdfbox-app-1.8.4.jar:gson-2.2.4.jar"
 
-
-javac -classpath $DEPS FillPDF.java
-java -classpath $DEPS:. FillPDF "$@"
+javac -classpath $DEPS FillPDF.java && java -classpath $DEPS:. FillPDF "$@"
