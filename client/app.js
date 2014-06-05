37f3b8bea816e790cc0f357400d3fc0ced49acd8
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-06-04 23:44:04 -0700

    client: translate into cz

--- client/app.js
+++ client/app.js
@@ -8,9 +8,9 @@ fm.service('loadingIndicator', LoadingIndicator);
 fm.config(function($routeProvider, $datepickerProvider) {
   $routeProvider
     .when('/',         {templateUrl: './menu.html'})
-    .when('/reserve',  {templateUrl: './reserve.html',  controller: ReserveController, resolve: ReserveController.resolve, title: 'Reserve Car'})
-    .when('/reserve/:id/success',  {templateUrl: './reserve_success.html',  controller: ReserveSuccessController, resolve: ReserveSuccessController.resolve, title: 'Success'})
-    .when('/existing', {templateUrl: './existing.html', controller: ExistingController, title: 'Existing Reservations'})
+    .when('/reserve',  {templateUrl: './reserve.html',  controller: ReserveController, resolve: ReserveController.resolve, title: 'Rezervace'})
+    .when('/reserve/:id/success',  {templateUrl: './reserve_success.html',  controller: ReserveSuccessController, resolve: ReserveSuccessController.resolve, title: 'Úspěšně zarezervováno'})
+    .when('/existing', {templateUrl: './existing.html', controller: ExistingController, title: 'Stávající rezervace'})
 
   angular.extend($datepickerProvider.defaults, {
     dateFormat: 'dd.MM.yyyy',
