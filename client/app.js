d83a07243b2a18fb5679ec7ab556ceafb469ccaf
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-06-15 08:28:15 -0700

    client: set up gulp build tasks

--- client/app.js
+++ client/app.js
@@ -7,10 +7,10 @@ fm.service('loadingIndicator', LoadingIndicator);
 
 fm.config(function($routeProvider, $datepickerProvider) {
   $routeProvider
-    .when('/',         {templateUrl: './menu.html'})
-    .when('/reserve',  {templateUrl: './reserve.html',  controller: ReserveController, resolve: ReserveController.resolve, title: 'Rezervace'})
-    .when('/reserve/:id/success',  {templateUrl: './reserve_success.html',  controller: ReserveSuccessController, resolve: ReserveSuccessController.resolve, title: 'Úspěšně zarezervováno'})
-    .when('/existing', {templateUrl: './existing.html', controller: ExistingController, title: 'Stávající rezervace'})
+    .when('/',         {templateUrl: 'menu.html'})
+    .when('/reserve',  {templateUrl: 'reserve.html',  controller: ReserveController, resolve: ReserveController.resolve, title: 'Rezervace'})
+    .when('/reserve/:id/success',  {templateUrl: 'reserve_success.html',  controller: ReserveSuccessController, resolve: ReserveSuccessController.resolve, title: 'Úspěšně zarezervováno'})
+    .when('/existing', {templateUrl: 'existing.html', controller: ExistingController, title: 'Stávající rezervace'})
 
   angular.extend($datepickerProvider.defaults, {
     dateFormat: 'dd.MM.yyyy',
---
+++ client/gulpfile.js
@@ -0,0 +1,121 @@
+// TODO(vojta): build css / less
+// TODO(vojta): add sha to built files
+// TODO(vojta): montage module loader?
+// TODO(vojta): replace libs with cdn / min-bundled versions
+
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var templateCache = require('gulp-angular-templatecache');
+var streamqueue = require('streamqueue');
+var htmlreplace = require('gulp-html-replace');
+var clean = require('gulp-clean');
+
+function multipleSrc() {
+  var args = Array.prototype.slice.call(arguments, 0)
+      .map(function(pattern) {
+        return gulp.src(pattern);
+      });
+
+  return streamqueue.apply(null, [{objectMode: true}].concat(args));
+}
+
+var paths = {
+  scripts: ['*.js', '!*_test.js', '!app.js', '!gulpfile.js', '!karma.conf.js', '!hacked-datapicker.js'],
+  templates: ['./*.html', '!index.html'],
+  init: ['app.js'],
+  datepicker: [
+    'bower_components/angular-strap/src/helpers/date-parser.js',
+    'bower_components/angular-strap/src/helpers/dimensions.js',
+    'bower_components/angular-strap/dist/modules/tooltip.js',
+    'hacked-datepicker.js',
+    'bower_components/angular-strap/dist/modules/datepicker.tpl.js'
+  ]
+};
+
+
+
+// Remove entire build folder.
+gulp.task('clean', function () {
+  return gulp.src('build', {read: false, force: false})
+    .pipe(clean());
+});
+
+
+
+// Compile Angular templates into a single JS file.
+gulp.task('build/templates.js', function () {
+  return gulp.src(paths.templates)
+    .pipe(templateCache({module: 'fm'}))
+    .pipe(gulp.dest('build'));
+});
+
+
+
+// Concat datepicker.
+gulp.task('build/datepicker.js', function() {
+  return gulp.src(paths.datepicker)
+    .pipe(concat('datepicker.js'))
+    .pipe(gulp.dest('build'));
+});
+
+
+
+// The entire app bundle.
+gulp.task('build/fm.js', ['build/templates.js', 'build/datepicker.js'], function() {
+  return multipleSrc(
+    paths.scripts,
+    'build/datepicker.js',
+    'app.js',
+    'build/templates.js'
+  ).pipe(concat('fm.js'))
+   .pipe(gulp.dest('build'));
+});
+
+
+
+// The entire app bundle - minified.
+gulp.task('build/fm.min.js', ['build/fm.js'], function() {
+  return gulp.src('build/fm.js')
+    .pipe(uglify())
+    .pipe(concat('fm.min.js'))
+    .pipe(gulp.dest('build'))
+});
+
+
+
+// The index.html with JS/CSS replaced.
+gulp.task('build/index', function() {
+  return gulp.src('index.html')
+    .pipe(htmlreplace({
+      'css': '../app.css',
+      'js' : './fm.js'
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+
+
+// The index.html with minified JS/CSS.
+gulp.task('build/index.min', function() {
+  return gulp.src('index.html')
+    .pipe(htmlreplace({
+        'css': '../app.css',
+        'js': './fm.min.js'
+    }))
+    .pipe(concat('index.min.html'))
+    .pipe(gulp.dest('build'));
+});
+
+
+
+// Copy all the assets.
+gulp.task('copy/assets', function() {
+  return gulp.src(['cars/*.jpg', 'loading.gif'], {base: '.'})
+  .pipe(gulp.dest('build'));
+})
+
+
+
+gulp.task('build', ['build/fm.js', 'build/index', 'copy/assets']);
