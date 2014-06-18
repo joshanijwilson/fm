5c6a8ce00257890efa69ea608e2028752a9954a2
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-06-17 21:41:23 -0700

    server: set up jasmine-node and add one test

---
+++ server/test/pdf_test.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+
+var pdfForms = require('../pdf_forms');
+var mysql = require('mysql');
+
+// TODO(vojta): Use testing db instead.
+var config = require('../config');
+var pool  = mysql.createPool(config.mysql);
+var diExpress = require('../di-express');
+
+
+// We pretend to run these tests in the root directory.
+process.chdir(path.normalize(__dirname + '/../..'));
+
+
+describe('pdf', function() {
+  it('should work', function(done) {
+    var query = diExpress.DbQuery(pool);
+    var spec = this;
+
+    pdfForms.scheduleGeneratingPdfForReservation(query, 74).then(function() {
+      expect(fs.existsSync('./storage/reservations/74/protocol.pdf')).toBe(true);
+      expect(fs.existsSync('./storage/reservations/74/survey.pdf')).toBe(true);
+    }, function(err) {
+      spec.fail(err);
+    }).done(function() {
+      pool.end();
+      done();
+    });
+  });
+});
