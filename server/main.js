2ef1fe372ec8f7372ff52249055e651b7d4e27da
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-07-26 22:30:02 -0700

    server: storage api for uploads

--- server/main.js
+++ server/main.js
@@ -19,6 +19,40 @@ app.get('/storage/reservations/:id-:document.pdf', function(req, res, next) {
 });
 
 
+// STORAGE - uploads.
+var fs = require('fs');
+var path = require('path');
+var busboy = require('connect-busboy');
+
+// Pseudo-random 16chars hex string.
+function randomHex() {
+  return Math.random().toString(16).slice(2, 8) + Math.random().toString(16).slice(2, 8) + Math.random().toString(16).slice(2, 6);
+}
+
+app.use(busboy());
+
+app.post('/storage/upload', function(req, res, next) {
+
+  req.pipe(req.busboy);
+  req.busboy.on('file', function(fieldname, file, filename) {
+
+    var storageId = randomHex() + path.extname(filename).toLowerCase();
+    var fstream = fs.createWriteStream(__dirname + '/../storage/uploads/' + storageId);
+
+    file.pipe(fstream);
+    fstream.on('close', function () {
+      res.setHeader('Location', '/storage/uploads/' + storageId);
+      res.end();
+    });
+  });
+});
+
+// TODO(vojta): Do heavy caching.
+app.get('/storage/uploads/:file', function(req, res, next) {
+  send(req, req.params.file, {root: __dirname + '/../storage/uploads'}).pipe(res);
+});
+
+
 // API routes, using DI.
 var api = require('./api');
 var diExpress = require('./di-express');
@@ -34,7 +68,7 @@ diExpress.registerRoutes(injector, router, api.routes);
 app.use('/api/v1', router);
 
 app.listen(config.port);
-
+console.log('port', config.port)
 function closeMySqlConnectionPool() {
   pool.end();
   process.exit(0);
