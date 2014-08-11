342acffc87c08fb7560172ebc8db61f4ba988495
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-08-10 17:45:00 -0700

    client/existing: show only user’s reservations
    Or all reservations if it is an admin user.

--- client/src/existing/existing_controller.js
+++ client/src/existing/existing_controller.js
@@ -1,6 +1,11 @@
-function ExistingController($scope, reservations) {
-  $scope.reservations = reservations;
-
+function ExistingController($scope, reservations, authUser) {
+  if (authUser.is_admin) {
+    $scope.reservations = reservations;
+  } else {
+    $scope.reservations = reservations.filter(function(reservation) {
+      return reservation.created_by === authUser.id;
+    });
+  }
 }
 
 ExistingController.resolve = {
@@ -10,7 +15,7 @@ ExistingController.resolve = {
 };
 
 
-ExistingController.$inject = ['$scope', 'dataReservations'];
+ExistingController.$inject = ['$scope', 'dataReservations', 'authUser'];
 ExistingController.resolve.dataReservations.$inject = ['dataSource'];
 
 module.exports = ExistingController;
