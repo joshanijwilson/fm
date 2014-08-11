38c2eafdec4aea7a6996398d11c8cd8a9cc247c3
Josh Anij Wilson <joshanijwilson@gmail.com>
2014-08-10 19:17:36 -0700

    client: show loading indicator for route changes

--- client/src/app_controller.js
+++ client/src/app_controller.js
@@ -3,7 +3,7 @@ var DEFAULT_PAGE_TITLE = 'FleetManager';
 function AppController($scope, loadingIndicator, $location, authUser) {
   $scope.pageTitle = DEFAULT_PAGE_TITLE;
 
-  // Update page title and hide loading indicator.
+  // Update page title.
   $scope.$on('$routeChangeSuccess', function(e, current) {
     if (!current) {
       $scope.pageTitle = DEFAULT_PAGE_TITLE;
@@ -14,9 +14,11 @@ function AppController($scope, loadingIndicator, $location, authUser) {
       $scope.pageHeader = current.$$route.title || DEFAULT_PAGE_TITLE;
       $scope.showMenuButton = angular.isDefined(current.$$route.showMenuButton) ? current.$$route.showMenuButton : true;
     }
-
-    loadingIndicator.hide();
   });
+
+  $scope.$on('$routeChangeStart', loadingIndicator.show);
+  $scope.$on('$routeChangeSuccess', loadingIndicator.hide);
+  $scope.$on('$routeChangeError', loadingIndicator.hide);
 
   // Initial page.
   // Show login form if the user is not authenticated.
--- client/src/loading_indicator.js
+++ client/src/loading_indicator.js
@@ -1,15 +1,26 @@
 function LoadingIndicator() {
   var backdrop = angular.element(document.getElementById('loader-indicator'));
   var body = angular.element(document.body);
+  var currentlyShown = false;
 
   this.show = function() {
+    if (currentlyShown) {
+      return;
+    }
+
     backdrop.addClass('fade in');
     body.addClass('modal-open');
+    currentlyShown = true;
   };
 
   this.hide = function() {
+    if (!currentlyShown) {
+      return;
+    }
+
     backdrop.removeClass('fade in');
     body.removeClass('modal-open');
+    currentlyShown = false;
   };
 }
 
