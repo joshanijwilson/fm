3c4f0dd0e5172896f0fc6bde28f57209912a69d7
Josh Anij Wilson <joshanijwilson@gmail.com>
2015-03-18 03:11:47 -0700

    chore: add scripts/install.sh
    To make the setup easier.

---
+++ scripts/install.sh
@@ -0,0 +1,41 @@
+# Requires:
+# - node, npm, bower, gulp
+# - mysqladmin, running MySQL
+# - gradle
+
+PROJECT_DIR=$(cd "$(dirname $(dirname "${BASH_SOURCE[0]}" ))" && pwd)
+
+echo $PROJECT_DIR
+exit 0
+
+# Install all NPM packages.
+for location in $PROJECT_DIR/*; do
+  if [ -e "$location/package.json" ]; then
+    echo "Installing NPM packages in $location"
+    cd $location
+    npm install
+  fi
+done
+
+# Install bower packages.
+cd $PROJECT_DIR/client
+bower install
+
+
+# Install DB.
+cd $PROJECT_DIR
+mysqladmin -u root -p create fleet_manager
+./db/node_modules/db-migrate/bin/db-migrate --config db/config.js --migrations-dir db/migrations/ up
+
+# Insert cars from csv.
+node scripts/import_cars_from_csv.js scripts/cars_filtered.csv
+
+
+# Build PDF generator.
+cd $PROJECT_DIR/pdf_generator
+gradle fatJar
+
+# Build client.
+cd $PROJECT_DIR/client
+gulp build build.min
+
