3e53f6c7db30c1953d476398f91b9095f20d9a37
Josh Anij Wilson <joshanijwilson@gmail.com>
2015-05-03 23:45:21 -0700

    client/existing: show finish button only to admins

--- client/src/existing/existing_controller.js
+++ client/src/existing/existing_controller.js
@@ -1,10 +1,12 @@
 function ExistingController($scope, reservations, authUser) {
   if (authUser.is_admin) {
     $scope.reservations = reservations;
+    $scope.showFinish = true;
   } else {
     $scope.reservations = reservations.filter(function(reservation) {
       return reservation.created_by === authUser.id;
     });
+    $scope.showFinish = false;
   }
 }
 
