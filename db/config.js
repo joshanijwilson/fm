b148ab3e836dcba46518857bd48615cf182c07ea
Josh Anij Wilson <joshanijwilson@gmail.com>
2015-05-03 20:04:14 -0700

    chore: explain weird db config

--- db/config.js
+++ db/config.js
@@ -1,10 +1,12 @@
+// This wraps the db config from ../config so that it can be consumed by db-migrate.
+
 var config = require('../config').forCurrentEnvironment();
 
 module.exports = {
   "dev": {
     "driver"   : "mysql",
     "user"     : config.mysql.user,
-    "password" : config.mysql.password,
+    "password" : config.mysql.password || "",
     "host"     : config.mysql.host,
     "database" : config.mysql.database
   }
